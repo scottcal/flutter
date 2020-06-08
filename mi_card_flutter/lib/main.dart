6184ed84c184e0fb105e0d8929dca90823454856
Scott Calafiore <scottcal@gmail.com>
2020-06-07 20:27:16 -0700

    mi card

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orangeAccent,
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/scott.jpg'),
+            ),
+            Text(
+              'Scott Calafiore',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
