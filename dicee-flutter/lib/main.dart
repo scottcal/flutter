e8e360721d105b8fb9cfaaa64c3bdc739c1b40a5
Scott Calafiore <scottcal@gmail.com>
2020-06-10 16:07:16 -0700

    Adding Buttons and On Press Event

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -18,15 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice3.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button pressed');
+              },
+              child: Image.asset('images/dice3.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
