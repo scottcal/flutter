303d6ac5a22f408af43cc5011fa97801b64b788e
Scott Calafiore <scottcal@gmail.com>
2020-06-11 20:48:29 -0700

    Added Stateful Widget

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -15,17 +15,25 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 4;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button got pressed');
+                setState(() {
+                  leftDiceNumber = 4;
+                  print('Left button got pressed');
+                });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
