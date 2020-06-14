12cf4c47774abc073c7feadf17d358905c7ad660
Scott Calafiore <scottcal@gmail.com>
2020-06-13 18:15:21 -0700

    Rebuild Magic 8 ball from scratch

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -12,10 +13,40 @@ class BallPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text('Ask me anything.'),
+        title: Text('Ask me anything'),
         backgroundColor: Colors.blue[900],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Ball(),
+        ],
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNum = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        focusColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onPressed: () {
+          setState(() {
+            ballNum = Random().nextInt(5) + 1;
+          });
+          print(ballNum);
+        },
+        child: Image.asset('images/ball$ballNum.png'),
+      ),
     );
   }
 }
